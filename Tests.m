46d904bf960990718aae1552e854b97f7e051c22
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2024-11-22 18:42:46 +0100

    Fix plotting

--- Tests.m
+++ Tests.m
@@ -22,12 +22,17 @@ end
 for n = n_values
   % Define the test problem
   [A, b, x, ProbInfo, m0] = defineTestProblem(model, n);
+
   m = size(A,1);
   % Find all non-zero singular vectors of sparce matrix A
   m_sv = min(size(A));
   fprintf('dims of A: %d x %d\n', size(A,1), size(A,2));
   assert(m0 <= m_sv, 'm0 must be less than or equal to the number of rows of A');
   fprintf('m = %d, m0 = %d\n', m, m0);
+
+  if model == "blurGauss"
+    A = sparse(full(A));
+  end
   [U,S,V] = svds(A, m_sv);
 
   % Plot singular values decay of A
@@ -49,20 +54,20 @@ for n = n_values
 
       [bn, NoiseInfo] = PRnoise(b, 'gauss', NoiseLevel(i));
       [X_gcv, X_opt, error_gcv, error_opt, k_gcv, k_opt] =...
-        gcv(U(:,1:m0), S(1:m0, 1:m0), V(:,1:m0), x, bn, m0, m);
-      testData = [testData; table(SNR(i), error_gcv, "gcv",...
+        gcv(U(:,1:m0), S(1:m0, 1:m0), V(:,1:m0), x, bn, m0, m, false);
+      testData = [testData; table(i, error_gcv, "gcv",...
                                   'VariableNames', {'SNR', 'Error', 'Method'})];
-      testData = [testData; table(SNR(i), error_opt, "opt",...
+      testData = [testData; table(i, error_opt, "opt",...
                                   'VariableNames', {'SNR', 'Error', 'Method'})];
-      kData = [kData; table(SNR(i), k_gcv, "gcv",...
+      kData = [kData; table(i, k_gcv, "gcv",...
                             'VariableNames', {'SNR', 'k', 'Method'})];
-      kData = [kData; table(SNR(i), k_opt, "opt",...
+      kData = [kData; table(i, k_opt, "opt",...
                             'VariableNames', {'SNR', 'k', 'Method'})];
     end
   end
   % Plot results
-  plotErrorResults(testData, SNR, n, m0, m);
-  plotKResults(kData, SNR, n, m0, m);
+  plotErrorResults(testData, n, m0, m);
+  plotKResults(kData, n, m0, m);
 
   % Display the reconstructions
   displayReconstructions(x, b, X_gcv, X_opt, ProbInfo);
@@ -83,45 +88,71 @@ function [A, b, x, ProbInfo, m0] = defineTestProblem(model, n)
     [A, b, x, ProbInfo] = PRtomo(n, options);
   elseif strcmp(model, 'blurGauss')
     options = IRset();
-    options.BlurLevel = 'mild';
+    options.BlurLevel = 'severe';
+    options.BC = 'zero';
+    options.trueImage = 'satellite';
+    options.CommitCrime = 'on';
     m0 = n*n;
-    [~, b, x, ProbInfo] = PRblurgauss(n, options);
-    A = psf2A(ProbInfo.psf, n);
+    [A, b, x, ProbInfo] = PRblurgauss(n, options);
   else
     error('Invalid model');
   end
 end
 
 % Function to plot results
-function plotErrorResults(testData, SNR, n, m0, m)
+function plotErrorResults(testData, n, m0, m)
   figure(10); clf;
-  testData.SNR = categorical(testData.SNR, SNR,...
-   {'1', '1e1', '1e2', '1e3', '1e4', '1e5', '1e6', '1e7', '1e8'});
-  boxchart(testData.SNR, testData.Error, 'GroupByColor', testData.Method);
-  title(['Results for n = ', num2str(n), ', ', num2str(m0), '/', num2str(m), ' singular values'],...
-        'interpreter', 'latex', 'fontsize', 18); 
-  ylabel('Relative error');
+
+  gcvData = testData(testData.Method == "gcv", :);
+  optData = testData(testData.Method == "opt", :);
+  title(['Results for n = ', num2str(n), ', ', num2str(m0), '/', num2str(m),...
+        ' singular values'], 'interpreter', 'latex', 'fontsize', 18);
+  %ylim(ax, ylim(ax) + [-1,1]*range(ylim(ax)).* 0.05)
+
   xlabel('SNR');
-  set(gca,'XTickLabels', {'1', '10^{1}', '10^{2}', '10^{3}', '10^{4}', '10^{5}', '10^{6}', '10^{7}', '10^{8}'});
+  set(gca,'XTickLabels',...
+    {'1' '10^{1}' '10^{2}' '10^{3}' '10^{4}' '10^{5}' '10^{6}' '10^{7}' '10^{8}'});
+    
+  ylabel('Relative error');
+  yyaxis left
+  boxchart(gcvData.SNR-0.25, gcvData.Error, 'GroupByColor', gcvData.Method, 'BoxWidth', 0.25);
+  %ylim([0, 0.5]);
+
+  % Plot second y-Axis with SNR for opt
+  yyaxis right
+  boxchart(optData.SNR+0.25, optData.Error, 'GroupByColor', optData.Method, 'BoxWidth', 0.25);
+  %ylim([0, 0.5]);
+
   ax = gca;
-  ax.YAxis.Scale ="log";
-  ylim(ax, ylim(ax) + [-1,1]*range(ylim(ax)).* 0.05)
+  ax.YAxis(1).Scale ="log";
+  ax.YAxis(2).Scale ="log";
+  %ax.YAxis.Scale ="log";
   legend()
 end
 
-function plotKResults(kData, SNR, n, m0, m)
+function plotKResults(kData, n, m0, m)
   figure(11); clf;
-  kData.SNR = categorical(kData.SNR, SNR,...
-   {'1', '1e1', '1e2', '1e3', '1e4', '1e5', '1e6', '1e7', '1e8'});
-  boxchart(kData.SNR, kData.k, 'GroupByColor', kData.Method);
+
+  gcvData = kData(kData.Method == "gcv", :);
+  optData = kData(kData.Method == "opt", :);
   title(['Results for n = ', num2str(n), ', ', num2str(m0), '/', num2str(m), ' singular values'],...
-        'interpreter', 'latex', 'fontsize', 18); 
-  ylabel('k');
+        'interpreter', 'latex', 'fontsize', 18);
+
   xlabel('SNR');
-  set(gca,'XTickLabels', {'1', '10^{1}', '10^{2}', '10^{3}', '10^{4}', '10^{5}', '10^{6}', '10^{7}', '10^{8}'});
+  set(gca,'XTickLabels',...
+    {'1' '10^{1}' '10^{2}' '10^{3}' '10^{4}' '10^{5}' '10^{6}' '10^{7}' '10^{8}'});
+  
+  ylabel('k');
+  yyaxis left
+  boxchart(gcvData.SNR-0.25, gcvData.k, 'GroupByColor', gcvData.Method, 'BoxWidth', 0.25);
+
+  % Plot second y-Axis with SNR for opt
+  yyaxis right
+  boxchart(optData.SNR+0.25, optData.k, 'GroupByColor', optData.Method, 'BoxWidth', 0.25);
+
   ax = gca;
-  ax.YAxis.Scale ="log";
-  ylim(ax, ylim(ax) + [-1,1]*range(ylim(ax)).* 0.05)
+  ax.YAxis(1).Scale ="log";
+  ax.YAxis(2).Scale ="log";
   legend()
 end
 
@@ -164,32 +195,4 @@ end
 % Helper function to save a figure
 function saveFigure(filename, figNumber)
   exportgraphics(figure(figNumber), filename);
-end
-
-function A = psf2A(psf, n)
-  % Signal Processing Toolbox required
-  A_full = convmtx2(psf, n, n);
-  % Calculate the output size of the full convolution
-  outRows = n + n - 1;
-  outCols = n + n - 1;
-
-  % Calculate the amount of padding
-  padRows = outRows - n;
-  padCols = outCols - n;
-
-  % Calculate starting and ending indices for rows and columns
-  rowStart = floor(padRows / 2) + 1;
-  rowEnd = rowStart + n - 1;
-
-  colStart = floor(padCols / 2) + 1;
-  colEnd = colStart + n - 1;
-
-  % Generate row indices for the 'same' convolution result
-  [sameRowIndices, sameColIndices] = ndgrid(rowStart:rowEnd, colStart:colEnd);
-  sameLinearIndices = sub2ind([outRows, outCols], sameRowIndices(:), sameColIndices(:));
-
-  % Extract the rows corresponding to the 'same' convolution
-  A = A_full(sameLinearIndices, :);
-
-  A = sparse(A);
 end
